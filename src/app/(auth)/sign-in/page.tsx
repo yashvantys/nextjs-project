d2ad55720f1728673bbc9d787269e08eb2ef9e6d
Yashvant Yadav <yashvanty@gmail.com>
2024-05-01 10:04:23 +0530

    sign in page added

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+'use client'
+
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
+    const { data: session } = useSession()
+    if (session) {
+        return (
+            <>
+                Signed in as {session.user.email} <br />
+                <button onClick={() => signOut()}>Sign Out</button>
+            </>
+        )
+    }
     return (
         <>
-            Not signed in <br />
+            not Signed in  <br />
+            <button onClick={() => signIn()}>Sign In</button>
         </>
     )
+
 }
+
+
+
